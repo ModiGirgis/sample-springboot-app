81bfe560c2a0e77dafbf640c19513f0d9b3312ce
Modi22 <medomig1234@gmail.com>
2020-12-16 15:33:37 -0500

    first commit of the spring security tutorial

---
+++ src/main/java/com/example/demo/SpringSecurityToAzureApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityToAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityToAzureApplication.class, args);
+	}
+
+}
